75c57cf73be6c9c6a648b35536d0caf81ccfb69e
bsqql123 <bsqql123@163.com>
2016-11-29 11:59:22 +0800

    修改初始化方式

--- src/cn/power/dao/Connection/MyConnection.java
+++ src/cn/power/dao/Connection/MyConnection.java
@@ -8,29 +8,26 @@ import java.sql.SQLException;
  * Created by power on 2016/11/23.
  */
 public class MyConnection {
-    public static String DBDRIVER;
-    public static String DBURL;
-    public static String DBUSER;
-    public static String DBPASS;
-    public MyConnection(){
-        DBDRIVER="org.gjt.mm.mysql.Driver";
-        DBURL= "jdbc:mysql://121.42.152.75:3306/power";
-        DBUSER="root";
-        DBPASS="bsqql123";
+    public static String DBDRIVER = "org.gjt.mm.mysql.Driver";
+    public static String DBURL = "jdbc:mysql://121.42.152.75:3306/power";
+    public static String DBUSER = "root";
+    public static String DBPASS = "bsqql123";
+
+    public MyConnection() {
     }
 
-    public  Connection getConnection(){
+    public Connection getConnection() {
         try {
             Class.forName(DBDRIVER);
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
         }
-        Connection conn =null;
+        Connection conn = null;
         try {
-            conn = DriverManager.getConnection(DBURL,DBUSER,DBPASS);
+            conn = DriverManager.getConnection(DBURL, DBUSER, DBPASS);
         } catch (SQLException e) {
             e.printStackTrace();
         }
-        return  conn;
+        return conn;
     }
 }
